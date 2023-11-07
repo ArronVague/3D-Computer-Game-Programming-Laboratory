2ecbcf77c22339af7d66d348a810869c68a87e90
SmileExpression <15018135093@163.com>
2023-11-07 22:28:57 +0800

    Add UFO prefab and basic files

---
+++ lab08/HitUFO/Assets/Script/UserGUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour
+{
+
+    SceneController CurrentSceneController;
+    GUIStyle msgStyle, titleStyle;
+    string roundText, bloodText, scoreText;
+
+    void Start(){
+        CurrentSceneController = Director.GetInstance().CurrentSceneController;
+
+        msgStyle = new GUIStyle();
+        msgStyle.normal.textColor = Color.black;
+        msgStyle.alignment = TextAnchor.MiddleCenter;
+        msgStyle.fontSize = 30;
+
+        titleStyle = new GUIStyle();
+        titleStyle.normal.textColor = Color.black;
+        titleStyle.alignment = TextAnchor.MiddleCenter;
+        titleStyle.fontSize = 60;
+    }
+
+    void OnGUI(){
+
+        roundText = $"Round: {CurrentSceneController.round}";
+        bloodText = $"Blood: {CurrentSceneController.blood}";
+        scoreText = $"Score: {CurrentSceneController.score}";
+
+        GUI.enabled = !CurrentSceneController.isStart;
+        // 重新开始的按钮
+        if(GUI.Button(new Rect(Screen.width*0.75f, Screen.height*0.85f, Screen.width*0.2f, Screen.height*0.1f), "Start")){
+            CurrentSceneController.StartNewGame();
+        }
+        // 标题与其他提示信息
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height*0.2f), "Hit UFO", titleStyle);
+        GUI.Label(new Rect(0, Screen.height*0.2f, Screen.width*0.15f, Screen.height*0.1f), roundText, msgStyle);
+        GUI.Label(new Rect(0, Screen.height*0.3f, Screen.width*0.15f, Screen.height*0.1f), bloodText, msgStyle);
+        GUI.Label(new Rect(0, Screen.height*0.4f, Screen.width*0.15f, Screen.height*0.1f), scoreText, msgStyle);
+        //判断是否结束游戏
+        if(CurrentSceneController.isFailed){
+            GUI.Label(new Rect(0, Screen.height*0.4f, Screen.width, Screen.height*0.2f), "Game Over.", titleStyle);
+        }
+    }
+}
