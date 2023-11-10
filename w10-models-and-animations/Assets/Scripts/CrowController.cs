3b1bcdceff78301a52dbda66b5784585fc81dbd0
SmileExpression <15018135093@163.com>
2023-11-10 11:39:47 +0800

    Complete lab 10's 7-01 to 7-06

--- w10-models-and-animations/Assets/Scripts/CrowController.cs
+++ w10-models-and-animations/Assets/Scripts/CrowController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class CrowController : MonoBehaviour
 {
+    private Animator ani;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ani = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        ani.SetBool("death", true);
     }
 }
