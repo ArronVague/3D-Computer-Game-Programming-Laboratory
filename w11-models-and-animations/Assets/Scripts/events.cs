23caa34ee22fedbd61b77794330eac32f420b1f0
SmileExpression <15018135093@163.com>
2023-11-17 11:30:53 +0800

    Create lab 11's code

---
+++ w11-models-and-animations/Assets/Scripts/events.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class events : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void JumpToTop()
+    {
+        Debug.Log("JumpToTop");
+    }
+}
