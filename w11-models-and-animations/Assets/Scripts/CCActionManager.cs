b0ab477c0dd1c3c445947f12ff860745339581a1
SmileExpression <15018135093@163.com>
2023-11-19 15:43:32 +0800

    Complete CCActionManager.cs

--- w11-models-and-animations/Assets/Scripts/CCActionManager.cs
+++ w11-models-and-animations/Assets/Scripts/CCActionManager.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CCActionManager : SSActionManager, ISSActionCallback {
+public class CCActionManager : SSActionManager, ISSActionCallback, IshootArrow {
 	
-	private FirstController sceneController;
-	private CCMoveToAction moveToA , moveToB, moveToC, moveToD;
+	public FirstController sceneController;
+	public ArrowFactory arrowFactory;
+	public play EmitArrow;
+	public GameObject Arrow;
+	public float force = 0f;
+	int count = 0;
 
 	protected new void Start() {
 		sceneController = (FirstController)SSDirector.getInstance ().currentSceneController;
 		sceneController.actionManager = this;
-		moveToA = CCMoveToAction.GetSSAction (new Vector3 (5, 0, 0), 1);
-		this.RunAction (sceneController.move1, moveToA, this);
-		moveToC = CCMoveToAction.GetSSAction (new Vector3 (-2, -2, -2), 1);
-		moveToD = CCMoveToAction.GetSSAction (new Vector3 (3, 3, 3), 1);
-		CCSequenceAction ccs = CCSequenceAction.GetSSAction (3, 0, new List<SSAction> {moveToC, moveToD});
-		this.RunAction (sceneController.move2, ccs, this);
+		arrowFactory = sceneController.arrowfactory;
 	}
 
 	// Update is called once per frame
@@ -24,6 +23,21 @@ public class CCActionManager : SSActionManager, ISSActionCallback {
 		base.Update ();
 	}
 		
+	public float getforce()
+	{
+		return force;
+	}
+
+	public void playArrow(Vector3 dir)
+	{
+		force = Random.Range(-50, 50);
+		EmitArrow = play.GetSSAction();
+		Arrow = arrowFactory.GetArrow1();
+		Arrow.transform.position = dir;
+		Arrow.GetComponent<Rigidbody>().AddForce(new Vector3(force, 0.3f, 2), ForceMode.Impulse);
+		this.RunAction(Arrow, EmitArrow, this);
+		Arrow.GetComponent<Data>().hit = false;
+	}
 	#region ISSActionCallback implementation
 	public void SSActionEvent (SSAction source, SSActionEventType events = SSActionEventType.Competeted, int intParam = 0, string strParam = null, Object objectParam = null)
 	{
