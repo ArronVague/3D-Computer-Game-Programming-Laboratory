dc36ea3e8225ab4d7b79d276b0409962bc73563c
SmileExpression <15018135093@163.com>
2023-11-19 20:52:30 +0800

    Update CCActionManager.cs

--- w11-models-and-animations/Assets/Scripts/CCActionManager.cs
+++ w11-models-and-animations/Assets/Scripts/CCActionManager.cs
@@ -38,17 +38,12 @@ public class CCActionManager : SSActionManager, ISSActionCallback, IshootArrow {
 		this.RunAction(Arrow, EmitArrow, this);
 		Arrow.GetComponent<Data>().hit = false;
 	}
-	#region ISSActionCallback implementation
+
 	public void SSActionEvent (SSAction source, SSActionEventType events = SSActionEventType.Competeted, int intParam = 0, string strParam = null, Object objectParam = null)
 	{
-		if (source == moveToA) {
-			moveToB = CCMoveToAction.GetSSAction (new Vector3 (-5, 0, 0), 1);
-			this.RunAction (sceneController.move1, moveToB, this);
-		} else if (source == moveToB) {
-			moveToA = CCMoveToAction.GetSSAction (new Vector3 (5, 0, 0), 1);
-			this.RunAction (sceneController.move1, moveToA, this);
-		}
+		arrowFactory.FreeArrow1(source.gameobject);
+		Debug.Log("free");
+		source.gameobject.GetComponent<Data>().hit = false;
 	}
-	#endregion
 }
 
