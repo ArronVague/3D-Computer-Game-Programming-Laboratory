64d209c5a5cf41eab2cf64f6797b89bada88ad7f
SmileExpression <15018135093@163.com>
2023-11-22 20:57:09 +0800

    Complete skybox switch

--- w11-models-and-animations/Assets/Scripts/SkyboxController.cs
+++ w11-models-and-animations/Assets/Scripts/SkyboxController.cs
@@ -3,20 +3,60 @@ using UnityEngine;
 public class SkyboxController : MonoBehaviour
 {
     public Material[] skyboxes;
-    private int currentSkyboxIndex = 0;
+    private int currentSkyboxIndex;
+    public bool runningSkybox = false;
+    public bool staticSkybox = false;
+    public LayerMask interactionLayerMask;
 
     // 其他脚本逻辑...
-    void Update()
+
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        currentSkyboxIndex = 0;
+        RenderSettings.skybox = skyboxes[currentSkyboxIndex];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            currentSkyboxIndex = (currentSkyboxIndex - 1 + skyboxes.Length) % skyboxes.Length;
-            RenderSettings.skybox = skyboxes[currentSkyboxIndex];
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            currentSkyboxIndex = (currentSkyboxIndex + 1) % skyboxes.Length;
-            RenderSettings.skybox = skyboxes[currentSkyboxIndex];
+            // 在按下"E"键时进行射线投射
+            Ray ray = new(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, interactionLayerMask))
+            {
+                // 如果射线击中了"E"对象，执行切换天空盒的逻辑
+                if (hit.collider.CompareTag("PressErunning"))
+                {
+                    if (!runningSkybox)
+                    {
+                        runningSkybox = true;
+                        currentSkyboxIndex = 1;
+                        
+                    }
+                    else
+                    {
+                        runningSkybox = false;
+                        currentSkyboxIndex = 0;
+                    }
+                    
+                }
+                else if (hit.collider.CompareTag("PressE")) {
+                    if (!staticSkybox)
+                    {
+                        staticSkybox = true;
+                        currentSkyboxIndex = 2;
+
+                    }
+                    else
+                    {
+                        staticSkybox = false;
+                        currentSkyboxIndex = 0;
+                    }
+                }
+                RenderSettings.skybox = skyboxes[currentSkyboxIndex];
+            }
         }
     }
 }
