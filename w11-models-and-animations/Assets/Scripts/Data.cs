5625f985227278594702d082b47c03f83831fcfa
SmileExpression <15018135093@163.com>
2023-11-22 13:05:00 +0800

    Delete useless speed

--- w11-models-and-animations/Assets/Scripts/Data.cs
+++ w11-models-and-animations/Assets/Scripts/Data.cs
@@ -7,7 +7,7 @@ public class Data : MonoBehaviour
 {
     public Vector3 size;
     public Color color = Color.red;
-    public float speed = 5f;
+/*    public float speed = 3f;*/
     public Vector3 director;
     public play Action;
     public bool hit;
