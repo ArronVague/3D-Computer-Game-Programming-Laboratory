047124e69bbd2f2649e499bdba19e16f264ddee1
SmileExpression <15018135093@163.com>
2023-11-23 14:00:36 +0800

    Add score display

--- w11-models-and-animations/Assets/Scripts/ArrowCollision.cs
+++ w11-models-and-animations/Assets/Scripts/ArrowCollision.cs
@@ -1,13 +1,18 @@
+using Com.Mygame;
+using System.Globalization;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class ArrowCollision : MonoBehaviour
 {
     private Quaternion initialRotation; // 保存箭的初始角度
     private bool hasCollided = false; // 碰撞标志位
+    public FirstController firstController;
 
     private void Start()
     {
         initialRotation = transform.rotation;
+        firstController = (FirstController)SSDirector.getInstance().currentSceneController;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -15,7 +20,7 @@ public class ArrowCollision : MonoBehaviour
         if (collision.gameObject.CompareTag("Target") && !hasCollided)
         {
             hasCollided = true;
-
+            firstController.score++;
             // 获取碰撞前的角度
             Quaternion preCollisionRotation = initialRotation;
 
--- w11-models-and-animations/Assets/Scripts/UserGUI.cs
+++ w11-models-and-animations/Assets/Scripts/UserGUI.cs
@@ -8,11 +8,13 @@ public class UserGUI : MonoBehaviour
 
     private IUserAction action;     // 用户动作接口
     private ISceneController queryInt;   // 场景接口
-    public bool isButtonDown = false;
     public new Camera camera;
+    public int score = 0;
+    public FirstController firstController;
 
     void Start()
     {
+        firstController = (FirstController)SSDirector.getInstance().currentSceneController;
         // 实例化对象
         action = SSDirector.getInstance().currentSceneController as IUserAction;
         queryInt = SSDirector.getInstance().currentSceneController as ISceneController;
@@ -20,23 +22,13 @@ public class UserGUI : MonoBehaviour
     void Update()
     {
     }
+
     void OnGUI()
     {
         GUIStyle fontstyle1 = new GUIStyle();
         fontstyle1.fontSize = 50;
         fontstyle1.normal.textColor = new Color(255, 255, 255);
-
-/*        if (Input.GetMouseButtonDown(0) && !isButtonDown)
-        {
-            Ray mouseRay = camera.ScreenPointToRay(Input.mousePosition);
-*//*            Debug.Log("RayDir = " + mouseRay.direction);
-            Debug.Log(action);*//*
-            action.hit();
-            isButtonDown = true;
-        }
-        else if (Input.GetMouseButtonDown(0) && isButtonDown)
-        {
-            isButtonDown = false;
-        }*/
+        // 绘制文本
+        GUI.Label(new Rect(0, 0, 400, 400), "score: " + firstController.score, fontstyle1);
     }
 }
